e66bf69f23e1a260a7a336775df80b9bdfbe6253
18alantom <2.alan.tom@gmail.com>
2025-07-07 13:53:08 +0530

    add license, update pyproject, version, build, etc

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from frappe_mcp.frappe_mcp.server.server import MCP
-from frappe_mcp.server.tools import Tool, ToolAnnotations
-
-__all__ = ['MCP', 'Tool', 'ToolAnnotations']
--- frappe_mcp/__init__.py
+++ frappe_mcp/__init__.py
@@ -3,3 +3,4 @@ from frappe_mcp.server.server import MCP
 from frappe_mcp.server.tools import Tool, ToolAnnotations
 
 __all__ = ['MCP', 'Tool', 'ToolAnnotations', 'server']
+__version__ = '0.1.0'
